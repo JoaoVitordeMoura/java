633be6de81bf1c164b1e89dab71714e69c21c3cc
João Vitor <161031038+JoaoVitordeMoura@users.noreply.github.com>
2025-03-07 10:34:18 -0300

    Add files via upload

--- menuCalculos/src/main/java/com/mycompany/menucalculos/MenuCalculos.java
+++ menuCalculos/src/main/java/com/mycompany/menucalculos/MenuCalculos.java
@@ -3,40 +3,54 @@ package com.mycompany.menucalculos;
 import java.util.Scanner;
 
 public class MenuCalculos {
+    private static Scanner tec = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner tec = new Scanner(System.in);
         int op;
-        
+
         do{
-            System.out.println("MENU");
-            System.out.println("--------");
-            System.out.println("1- Oposto");
-            System.out.println("2- Sinal");
-            System.out.println("3- Sair");
-            System.out.print("Opção: ");
-            op = Integer.parseInt(tec.nextLine());
-        
+            op = menu();
             switch(op){
                 case 1:
-                    System.out.print("Digite um número: ");
-                    double n = Double.parseDouble(tec.nextLine());
-                    n = n - n * 2;
-                    System.out.println("O oposto é: " + n);
+                    oposto();
                     break;
                 case 2:
-                    System.out.print("Digite um número: ");
-                    n = Double.parseDouble(tec.nextLine());
-                    if(n > 0){
-                        System.out.println("O número é positivo");
-                    }else if(n < 0){
-                        System.out.println("O número é negativo");
-                    }else{
-                        System.out.println("Nulo");
-                    }
-                    break;
-                case 3:
+                    sinal();
                     break;
             }
         }while(op != 3);
     }
+    
+    private static int menu(){
+        System.out.println("MENU");
+        System.out.println("--------");
+        System.out.println("1- Oposto");
+        System.out.println("2- Sinal");
+        System.out.println("3- Sair");
+        System.out.print("Opção: "); 
+        return Integer.parseInt(tec.nextLine());
+    }
+    
+    private static void oposto(){
+        double n = input("Digite um número: ");
+//      n = n - n * 2;
+        System.out.println("O oposto é: " + -n);
+    }
+    
+    private static void sinal(){
+        double n = input("Digite um número: ");
+        if(n > 0){
+            System.out.println("O número é positivo");
+        }else if(n < 0){
+            System.out.println("O número é negativo");
+        }else{
+            System.out.println("Nulo");
+        }
+    }
+    
+    private static double input(String msg){
+        System.out.print(msg);
+        return Double.parseDouble(tec.nextLine());
+    }
+    
+     
 }
